84ab818e5c84812c924f39086c15929c725984b3
anggaariap <anggaariap@gmail.com>
2024-08-06 08:58:24 +0700

    update navbar for responsive mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
   return (
     <nav className="navbar">
-      <img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" alt="IMDB Logo" />
-      <NavLink exact to="/" activeClassName="active">
-        Home
-      </NavLink>
-      <NavLink to="/popular" activeClassName="active">
-        Popular Movies
-      </NavLink>
-      <NavLink to="/search" activeClassName="active">
-        Search
-      </NavLink>
+      <div className="navbar-header">
+        <img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" alt="IMDB Logo" />
+      </div>
+      <div className="navbar-links">
+        <NavLink exact to="/" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/popular" activeClassName="active">
+          Popular Movies
+        </NavLink>
+        <NavLink to="/search" activeClassName="active">
+          Search
+        </NavLink>
+      </div>
     </nav>
   );
 };
